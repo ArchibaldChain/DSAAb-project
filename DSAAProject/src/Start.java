284bf84cc89550fa23fddca00e954675c974c9c9
readypeng <543665207@qq.com>
2018-12-16 20:35:19 +0800

    无

--- DSAAProject/src/Start.java
+++ DSAAProject/src/Start.java
@@ -1,11 +1,8 @@
+import DataAnalysis.BornPlace;
 import Student.CSVReader;
 import Student.Student;
-import DataAnalysis.BornPlace;
-
 
 import java.util.ArrayList;
-import java.util.TreeMap;
-import java.util.TreeSet;
 
 /**
  * ProjectName: DSAAProject
@@ -16,11 +13,11 @@ import java.util.TreeSet;
  */
 public class Start {
     public static void main(String[] args) {
-        CSVReader reader = new CSVReader("FileStorage\\Project_data_20181208.csv"); // Use the relative path of the .csv file
+        CSVReader reader = new CSVReader("DSAAProject\\FileStorage\\Project_data_20181208.csv"); // Use the relative path of the .csv file
         ArrayList<Student> students = reader.parse(); // Format the data into a list
 
         BornPlace bornPlace = new BornPlace(students.toArray(new Student[0]));
-        FileIO.fileWriter(bornPlace.toString(),"FileStorage/born place.txt");
+        FileIO.fileWriter(bornPlace.toString(), "DSAAProject\\FileStorage\\born place.txt");
     }
 }
 
